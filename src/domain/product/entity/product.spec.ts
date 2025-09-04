de394cf4838451e4c3b8e9ab5f509ff03b97a121
Bruno Correa <brunosantos6ft@gmail.com>
2025-09-03 21:13:09 -0300

    feat: implement notification pattern on product entity

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -5,21 +5,30 @@ describe("Product unit tests", () => {
     expect(() => {
       const product = new Product("", "Product 1", 100);
       return product;
-    }).toThrow("Id is required");
+    }).toThrow("product: Id is required");
   });
 
   it("should throw error when name is empty", () => {
     expect(() => {
       const product = new Product("1", "", 100);
       return product;
-    }).toThrow("Name is required");
+    }).toThrow("product: Name is required");
   });
 
   it("should throw error when price is less zero", () => {
     expect(() => {
       const product = new Product("1", "Product 1", -1);
       return product;
-    }).toThrow("Price must be greater than zero");
+    }).toThrow("product: Price must be greater than zero");
+  });
+
+  it("should throw error when id, name and price are invalid", () => {
+    expect(() => {
+      const product = new Product("", "", -1);
+      return product;
+    }).toThrow(
+      "product: Id is required,product: Name is required,product: Price must be greater than zero"
+    );
   });
 
   it("should change name", () => {
